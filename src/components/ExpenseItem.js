7cb158492b84ca878b3f77a5e3a80a0d897ebdbc
ankitdhinwa <akdhinwa91@gmail.com>
2023-12-24 17:17:06 +0530

    Dynamic Data Outputting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
-    return <h2>This is an Expense Item!!</h2>
+    const ExpenseDate= new Date(2023, 11, 25);
+    const ExpenseDesc = 'Automobile Pvt. Ltd.'
+    const ExpensePrice = '2,81,60,305'
+    return <div className='expense-item'>
+            <div>{ExpenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{ExpenseDesc}</h2>
+                <div className='expense-item__price'>{ExpensePrice}₹</div>
+            </div>
+    </div>
 }
 
 export default ExpenseItem;
