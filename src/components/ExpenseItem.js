166fa4fa2901fcebd1351825ae9a5ebac9146ed1
ankitdhinwa <akdhinwa91@gmail.com>
2023-12-25 18:43:58 +0530

    State introduced

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title);
+  
   const clickHandler= ()=>{
-    console.log("Clicked!!");
+    setTitle("Oil Corporation");
+    //console.log(title);
   };
   return (
     <Card className="expense-item">
         <ExpenseDate edate={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}₹</div>
         <button onClick={clickHandler}>Click Me</button>
         {/* clickHandler function is called as clickHandler only not as clickHandler() as it is now executed only when event occurs.When clickhandler() is used then it act as a JS and executed when Card is started executing.*/}
