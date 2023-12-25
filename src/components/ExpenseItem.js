7c96381d65a67c68b2c4986fc0120d34f348feb9
ankitdhinwa <akdhinwa91@gmail.com>
2023-12-25 18:15:35 +0530

    EventListener Introduced

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,12 +3,17 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+  const clickHandler= ()=>{
+    console.log("Clicked!!");
+  };
   return (
     <Card className="expense-item">
         <ExpenseDate edate={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}₹</div>
+        <button onClick={clickHandler}>Click Me</button>
+        {/* clickHandler function is called as clickHandler only not as clickHandler() as it is now executed only when event occurs.When clickhandler() is used then it act as a JS and executed when Card is started executing.*/}
       </div>
     </Card>
   );
