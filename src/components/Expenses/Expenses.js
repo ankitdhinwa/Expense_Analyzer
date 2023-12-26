0f84c9ce107f9ccfc711c0d3b317791710d66e8e
ankitdhinwa <akdhinwa91@gmail.com>
2023-12-26 19:50:23 +0530

    Rendering List Dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,45 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
-
-function Expenses(props){
-  const [filteredYear, setFilteredYear] = useState('2023');
-  const changeFilterHandler = (yearSelected)=>{
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2023");
+  const changeFilterHandler = (yearSelected) => {
     setFilteredYear(yearSelected);
-  }
+  };
   return (
-      <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
-          <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-          ></ExpenseItem>
-          <ExpenseItem
-            title={props.items[4].title}
-            amount={props.items[4].amount}
-            date={props.items[4].date}
-          ></ExpenseItem>
-        </Card>
-      );
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {/* Accessing ExpenseItems dynamically rather by indexing  */}
+      {props.items.map((expenses) => (
+        <ExpenseItem
+          title={expenses.title}
+          amount={expenses.amount}
+          date={expenses.date}
+        />
+      ))}
+    </Card>
+  );
 }
 
 export default Expenses;
